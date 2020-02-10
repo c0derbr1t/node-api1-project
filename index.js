d5eafd4db85aa71cf47ae80dffabe4e89c119e78
Lambda_School_Loaner_252 <c0der.br1t@gmail.com>
2020-02-10 15:18:11 -0600

    Initial Commit. Installed dependencies. Created basic server with Express. Created GET request to /api/users.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // implement your API here
+const express = require('express');
+
+const Lambda = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req,res) => {
+    res.json({ message: 'Welcome to Lambda' });
+})
+
+// View users
+server.get('/api/users', (req, res) => {
+    Lambda.find().then(item => {
+        res.status(200).json(item);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+    })
+})
+
+const port = 5000;
+server.listen(port, () => console.log(`Server listening on port ${port}!`));
